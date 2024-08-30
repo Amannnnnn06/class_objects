80262a52b51c94ef75e54ce042c58eb3f6bd1c95
Amannnnnn06 <amannnnnn1405@gmail.com>
2024-08-30 20:46:25 +0530

    Add files via upload

---
+++ outsideclass.cpp
@@ -0,0 +1,23 @@
+//Name:Aman Prajapati
+//PRN:23070123017
+//Aim:Implemention of Classes and Objects to Define Method outside Class
+
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    public:
+    void myMethod();
+};
+
+void Student::myMethod()
+{
+    cout<<"A Sophomore in SIT";
+}
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
